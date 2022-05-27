72665638297fb38bfa624c05c7efdf8fe9e876dc
BakdauletBolatE <bakdauletbolat@MacBook-Air-Bakdaulet.local>
2022-05-28 00:29:53 +0600

    added persen model admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Brand,Project,Feature,Rating,Request
+from .models import Brand, Person,Project,Feature,Rating,Request
 
 admin.site.register(Brand)
 admin.site.register(Project)
 admin.site.register(Feature)
 admin.site.register(Rating)
 admin.site.register(Request)
+admin.site.register(Person)
