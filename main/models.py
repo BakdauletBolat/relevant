d52a3f285914a01f904acdb80de8068ad457de45
Lisa <bakosh21345@gmail.com>
2022-12-15 18:52:44 +0600

    a

--- main/models.py
+++ main/models.py
@@ -1,94 +1,82 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Brand(models.Model):
-
     name = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='brands/')
 
     class Meta:
-
         verbose_name = 'brand'
         verbose_name_plural = 'brands'
-    
+
     def __str__(self):
         return self.name
 
 
 class TelegramUser(models.Model):
-
     chat_id = models.CharField(max_length=255)
     user_name = models.CharField(max_length=255)
 
-class Project(models.Model):
 
+class Project(models.Model):
     name = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='projects/')
     short_description = models.TextField()
-    description = models.TextField(null=True,blank=True)
-
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
-
         verbose_name = 'project'
         verbose_name_plural = 'projects'
-    
+
     def __str__(self):
         return self.name
 
 
 class Feature(models.Model):
-
     name = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='features/')
     short_description = models.TextField()
     icon_name = models.CharField(max_length=255)
 
-
     class Meta:
-
         verbose_name = 'Feature'
         verbose_name_plural = 'Features'
-    
+
     def __str__(self):
         return self.name
 
 
 class Rating(models.Model):
-
-    star = models.CharField(default="4",max_length=255)
+    star = models.CharField(default="4", max_length=255)
     full_name = models.CharField(max_length=255)
     photo_user = models.ImageField(upload_to='photo-user/')
-    description = models.TextField(null=True,blank=True)
-
+    description = models.TextField(null=True, blank=True)
 
     class Meta:
-
         verbose_name = 'Rating'
         verbose_name_plural = 'Ratings'
-    
+
     def __str__(self):
         return self.full_name
 
-class Request(models.Model):
 
+class Request(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255,verbose_name='Телефон номера')
+    phone = models.CharField(max_length=255, verbose_name='Телефон номера')
     created_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.name
 
 
 class Person(models.Model):
-
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255,verbose_name='Телефон номера')
+    phone = models.CharField(max_length=255, verbose_name='Телефон номера')
     photo = models.ImageField(upload_to='features/')
-    instagram = models.CharField(max_length=255,verbose_name='Instagram')
-    position = models.CharField(max_length=255,verbose_name='Должность')
+    instagram = models.CharField(max_length=255, verbose_name='Instagram')
+    position = models.CharField(max_length=255, verbose_name='Должность')
 
-    
     def __str__(self):
         return self.name
--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,13 @@
-from .views import (home_view,contact_us_view,submit_request,
-                    about_us_view,projects_view,success_view,TelegramUserCreateAPIView)
+from .views import (home_view, contact_us_view, submit_request,
+                    about_us_view, projects_view, success_view, TelegramUserCreateAPIView)
 from django.urls import path
 
 urlpatterns = [
-    path('', home_view,name='home_view'),
-    path('contact-us',contact_us_view,name='contact_us_view'),
-    path('submit-request',submit_request,name='submit_request'),
-    path('about-us',about_us_view,name='about_us_view'),
-    path('projects',projects_view,name='projects_view'),
-    path('success',success_view,name='success_view'),
-    path('api/v1/create-telegram-bot',TelegramUserCreateAPIView.as_view())
+    path('', home_view, name='home_view'),
+    path('contact-us', contact_us_view, name='contact_us_view'),
+    path('submit-request', submit_request, name='submit_request'),
+    path('about-us', about_us_view, name='about_us_view'),
+    path('projects', projects_view, name='projects_view'),
+    path('success', success_view, name='success_view'),
+    path('api/v1/create-telegram-bot', TelegramUserCreateAPIView.as_view())
 ]
-
